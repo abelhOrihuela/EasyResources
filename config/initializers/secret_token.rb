cbfe2e7c9ec62b826a1240d01ed4dc9e45d60a85
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-05-25 19:49:54 -0500

    App Base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyMovies::Application.config.secret_key_base = '4bdace56f2c7b5dcabd95f2a7e25e0eb1443b92dd9a62dc517d42ccfc92141f1008c4ff1b2442cb06908c2f8dc02a46c017227ffb149433533b31ea488517165'
