4618cebbead3ffa035517248ae57ded533f50993
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-05-25 19:58:48 -0500

    Disable generate assets

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+config.generators do |g|
+  g.assets false
+end
+
 module EasyMovies
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
